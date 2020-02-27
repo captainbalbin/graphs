cc5c7120497e88a516f8b385ebec9a5f71b03ef0
Albin Sjöstrand <albin.sjostrand@decerno.se>
2020-02-27 13:51:23 +0100

    removed billboard, using chart js

--- public/billboardScript.js
+++
@@ -1,8 +0,0 @@
-// Scale #html_text from size specified for 1000 viewport width to current width
-function resizeHTMLText() {
-  var w = d3.select('#sketch1').node().clientWidth;
-  d3.selectAll('.html_overlay').style('transform', 'scale(' + w / 1000 + ')');
-}
-
-resizeHTMLText();
-window.addEventListener('resize', resizeHTMLText);
