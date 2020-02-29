33d3caba6a53b06de35f522df397614562b80632
Albin Sjöstrand <albin.sjostrand@decerno.se>
2020-02-29 18:49:14 +0100

    removed unused git code

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const path = require('path');
+
+//denna behöver du egentligen inte
 const logger = require('./middleware/logger');
 
 const app = express();
 
-// Init middleware
+// Init middleware (och inte den här heller)
 app.use(logger);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// A way to do it but not the best way
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
+
+
+
